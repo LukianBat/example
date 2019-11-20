caa676120aafd6638896bb8648d09094fbd7daee
Oleg Mineev <o.mineev@movavi.com>
2019-11-20 17:49:56 +0700

    sharedviewmodel - for change data between Fragments

--- app/src/main/java/com/movavi/android/geophysics/MainActivity.kt
+++ app/src/main/java/com/movavi/android/geophysics/MainActivity.kt
@@ -3,7 +3,9 @@ package com.movavi.android.geophysics
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import com.movavi.android.geophysics.core.SharedViewModel
 import com.movavi.android.geophysics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val navigation = this.findNavController(R.id.myNavHostFragment)
+        val sharedViewModel = ViewModelProviders.of(this).get(SharedViewModel::class.java)
     }
 
 }
