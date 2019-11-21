f595b4f666facd7124da77a470c2fc422aa3747a
Lukyan Zhukov <l.zhukov@movavi.com>
2019-11-21 12:21:24 +0700

    + GetResUseCase
    Добавлен статический метод для отправки и получения данных из MyMath
    
    #1 #6

--- app/src/main/java/com/movavi/android/geophysics/core/Res.kt
+++ app/src/main/java/com/movavi/android/geophysics/core/Res.kt
@@ -1,5 +1,3 @@
 package com.movavi.android.geophysics.core
 
-data class ResItem(val name: String, val corel: Float, val regres: String)
-
-data class ResList(private val list: List<ResItem>)
+data class ResItem(val name: String, val corel: Double, val regres: String)
