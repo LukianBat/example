b9946cecced4719e351609bea273660243e1660a
ivincetti <ivincetti@gmail.com>
2019-11-24 12:57:17 +0700

    downloading error fragment logic

--- app/src/main/java/com/movavi/android/geophysics/presentation/downloading/DownloadingErrorFragment.kt
+++ app/src/main/java/com/movavi/android/geophysics/presentation/downloading/DownloadingErrorFragment.kt
@@ -7,14 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.fragment.findNavController
 import com.movavi.android.geophysics.R
 import com.movavi.android.geophysics.core.SharedViewModel
-import com.movavi.android.geophysics.data.ResultsAdapter
 import com.movavi.android.geophysics.databinding.FragmentDownloadErrorBinding
-import com.movavi.android.geophysics.databinding.FragmentResultBinding
 
 /**
  * Fragment for network error animation.
@@ -25,15 +22,23 @@ class DownloadingErrorFragment : Fragment() {
 
     private lateinit var sharedViewModel: SharedViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_download_error,
-            container,false)
+            container, false
+        )
 
         sharedViewModel = ViewModelProviders.of(activity!!).get(SharedViewModel::class.java)
+
+        binding.lavError.setOnClickListener {
+            this.findNavController().navigate(R.id.action_downloadingErrorFragment_to_startFragment)
+        }
+
         return binding.root
     }
 }
